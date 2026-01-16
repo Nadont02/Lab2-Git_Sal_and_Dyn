4257d24e241048cf1965d60f6191975f5fc5a6d8
Nadont02 <dunyushkin2007@mail.ru>
2026-01-16 15:41:42 +0300

    Add user name input and greeting

--- Program/Program.cs
+++ Program/Program.cs
@@ -5,5 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        string? name = Console.ReadLine();
+        Console.WriteLine($"Hello {name}");
     }
 }
